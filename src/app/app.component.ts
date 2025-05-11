671cf551d30cbf67686a149fc61a47dde00b500e
Gean G. Carneiro <geancarneiro1@gmail.com>
2025-05-11 06:01:09 -0300

    Evolução top a acessibilidade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import {A11yModule, LiveAnnouncer } from '@angular/cdk/a11y';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,9 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'counter';
+
+  @ViewChildren('counterRow') counterRows: QueryList<ElementRef>;
+  @ViewChild('filtroElem') filtroElem: ElementRef;
 
   readonly counterList : Counter[] = [
     new Counter("Semáforo Verde", 15),
@@ -34,74 +37,101 @@ export class AppComponent {
     new Counter("Saltar o Próximo Jogador", 2)
   ];
 
-  tecla : string;
-
   filter : string = '';
 
-  selectedCounter = -1;
+  constructor(private announcer: LiveAnnouncer) {
+  }
 
   @HostListener('document:keydown', ['$event'])
   doCommand(evt : KeyboardEvent){
-    
-
     if(evt.ctrlKey) {
 
     } else {
-      this.tecla = evt.key;
       switch(evt.key) {
-        
+
         case 'ArrowDown':
           evt.preventDefault();
           evt.stopPropagation();
-          this.selectedCounter = (this.selectedCounter + 1) % this.filteredCounterList.length;
+          if(this.selectedCounter > -1) {
+            let selectedCounter = (this.selectedCounter + 1) % this.filteredCounterList.length;
+            this.counterRows.toArray()[selectedCounter]?.nativeElement.focus();
+          } else {
+            this.counterRows.first.nativeElement.focus();
+          }
           break;
         case 'ArrowUp':
           evt.preventDefault();
           evt.stopPropagation();
-          this.selectedCounter = (this.selectedCounter - 1 + this.filteredCounterList.length) % this.filteredCounterList.length;
+          if(this.selectedCounter > -1) {
+            let selectedCounter = (this.selectedCounter - 1 + this.filteredCounterList.length) % this.filteredCounterList.length;
+            this.counterRows.toArray()[selectedCounter]?.nativeElement.focus();
+          } else {
+            this.counterRows.last.nativeElement.focus();
+          }
           break;
         case 'ArrowLeft':
           evt.preventDefault();
           evt.stopPropagation();
-          this.filteredCounterList[this.selectedCounter].count = Math.max(0, this.filteredCounterList[this.selectedCounter].count - 1);
+          this.filteredCounterList[this.selectedCounter].count = Math.max(0, this.selectedCounterCount - 1);
           break;
         case 'ArrowRight':
           evt.preventDefault();
           evt.stopPropagation();
-          this.filteredCounterList[this.selectedCounter].count = Math.min(this.filteredCounterList[this.selectedCounter].max, this.filteredCounterList[this.selectedCounter].count + 1);
+          this.filteredCounterList[this.selectedCounter].count = Math.min(this.filteredCounterList[this.selectedCounter].max, this.selectedCounterCount + 1);
           break;
         case 'Escape':
           evt.preventDefault();
           evt.stopPropagation();
           if(this.filter === '') {
             if(this.selectedCounter > -1){
-              this.selectedCounter = -1;
+              (document.activeElement as HTMLElement).blur();
             } else {
               this.counterList.forEach( value => value.count = 0 );
             }
-            
+
           } else {
             this.filter = '';
           }
           break;
-        default:
-          if(isNaN(+evt.key)){
-            this.selectedCounter = -1;
-            if(evt.key.length === 1){
-              this.filter += evt.key;
-            } else {
-              switch(evt.key) {
-                case 'Backspace':
-                  this.filter = this.filter.slice(0, -1);
-                  break;
-              }
-            }
+        case 'f':
+          evt.preventDefault();
+          evt.stopPropagation();
+          if(this.filtroElem.nativeElement !== document.activeElement) {
+            this.filtroElem.nativeElement.focus();
           }
+          break;
+        default:
+
           break;
       }
     }
-    
+
   }
+
+  get selectedCounter(): number {
+    return !this.counterRows ? -1 : this.counterRows.toArray().indexOf(this.counterRows.find(item => item.nativeElement === document.activeElement) );
+  }
+
+  get selectedCounterCount(): number {
+    return this.filteredCounterList[this.selectedCounter]?.count ?? 0;
+  }
+
+  rowToText(row : HTMLTableRowElement): string {
+    if (row) {
+      const cells = Array.from(row.getElementsByTagName('td'));
+      return cells.map(cell => cell.textContent).join(' ');
+    }
+    return '';
+  }
+
+
+  get selectedRow(): HTMLTableRowElement | null {
+    if (this.counterRows) {
+      return this.counterRows.toArray()[this.selectedCounter]?.nativeElement;
+    }
+    return null;
+  }
+
 
   get filteredCounterList() {
     return this.counterList.filter(value => this.normalizeStr(value.label).includes(this.normalizeStr(this.filter)))
@@ -109,6 +139,14 @@ export class AppComponent {
 
   normalizeStr(str: string): string {
     return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
+  }
+
+  broadcastMsg(msg:string) {
+
+    this.announcer.announce(msg, 'assertive');
+    //setTimeout(() => {
+    //  this.announcer.clear();
+    //}, 1000);
   }
 
 }
